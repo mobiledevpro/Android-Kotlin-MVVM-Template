1476d1fc22925110ceb09e1447fb00a55dc8e16b
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2017-10-20 19:10:49 +0300

    Chnages

--- data/src/main/java/com/mobiledevpro/data/Constants.java
+++ data/src/main/java/com/mobiledevpro/data/Constants.java
@@ -1,12 +1,10 @@
-package com.mobiledevpro.apptemplate.helper;
-
-import com.mobiledevpro.apptemplate.BuildConfig;
+package com.mobiledevpro.data;
 
 /**
- * All constants for UI
+ * All constants for the app
  */
 
-public class UiConstants {
+public class Constants {
     public static final String LOG_TAG_DEBUG = BuildConfig.APPLICATION_ID + ".debug";
     public static final String LOG_TAG_ERROR = BuildConfig.APPLICATION_ID + ".error";
 }
