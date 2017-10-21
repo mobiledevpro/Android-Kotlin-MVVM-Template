8b660380d43948a8b976ec029b1ca413645d9b59
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2017-10-21 15:11:32 +0300

    Changed fragments helper

--- app/src/main/java/com/mobiledevpro/apptemplate/helper/FragmentsHelper.java
+++ app/src/main/java/com/mobiledevpro/apptemplate/helper/FragmentsHelper.java
@@ -5,15 +5,18 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
+import com.mobiledevpro.apptemplate.R;
 import com.mobiledevpro.apptemplate.ui.mainscreen.view.MainFragment;
 
 /**
  * Helper class for work with fragments
  * <p>
- * Created by Dmitriy V. Chernysh on 23.01.17.
+ * Created by Dmitriy V. Chernysh
  * dmitriy.chernysh@gmail.com
  * <p>
- * www.mobile-dev.pro
+ * https://fb.me/mobiledevpro/
+ * <p>
+ * #MobileDevPro
  */
 
 public class FragmentsHelper {
@@ -39,4 +42,36 @@ public class FragmentsHelper {
         ).commit();
     }
 
+    /**
+     * Show fragment for customers list
+     *
+     * @param fm FragmentManager
+     */
+    public static void showMainFragmentWithAnimation(FragmentManager fm, @IdRes int containerResId, boolean addToBackStack) {
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.setAllowOptimization(false);
+
+        if (addToBackStack) {
+            ft.setCustomAnimations(
+                    R.anim.base_anim_fragment_enter_slide_left,
+                    R.anim.base_anim_fragment_exit_slide_right,
+                    R.anim.base_anim_fragment_pop_enter_slide_right,
+                    R.anim.base_anim_fragment_pop_exit_slide_left
+            );
+            ft.addToBackStack(TAG_FRAGMENT_MAIN);
+        } else {
+            ft.setCustomAnimations(
+                    0,
+                    R.anim.base_anim_fragment_exit_slide_right,
+                    R.anim.base_anim_fragment_pop_enter_slide_right,
+                    0
+            );
+        }
+
+        ft.replace(
+                containerResId,
+                MainFragment.newInstance(),
+                TAG_FRAGMENT_MAIN
+        ).commit();
+    }
 }
