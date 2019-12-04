9325dc54387f9f6788990f58c5eae7232188b97d
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2019-12-04 20:44:00 +0200

    Updated gradle files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-echo y | android update sdk --filter "build-tools-27.0.3,android-27,extra-android-m2repository" --no-ui -a # Grab the Android Support Repo which isn't included in the container
+echo y | android update sdk --filter "build-tools-29.0.2,android-29,extra-android-m2repository" --no-ui -a # Grab the Android Support Repo which isn't included in the container
 mkdir "${ANDROID_HOME}/licenses" || true
 echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "${ANDROID_HOME}/licenses/android-sdk-license"
 
