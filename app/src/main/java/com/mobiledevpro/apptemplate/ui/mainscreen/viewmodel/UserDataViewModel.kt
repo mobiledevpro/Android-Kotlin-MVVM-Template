2a94bc55c528573537843444379384ce85f6e6c2
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2019-12-06 16:05:17 +0200

    Change button state

--- app/src/main/java/com/mobiledevpro/apptemplate/ui/mainscreen/viewmodel/UserDataViewModel.kt
+++ app/src/main/java/com/mobiledevpro/apptemplate/ui/mainscreen/viewmodel/UserDataViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.mobiledevpro.apptemplate.Event
 import com.mobiledevpro.data.LOG_TAG_DEBUG
@@ -12,6 +13,7 @@ import com.mobiledevpro.interactor.useredit.UserEditInteractor
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
+
 
 /**
  * ViewModel for user data
@@ -25,11 +27,11 @@ import io.reactivex.rxkotlin.subscribeBy
  */
 class UserDataViewModel(app: Application) : AndroidViewModel(app) {
 
+    private val _cachedUserName = MutableLiveData<String>()
+    private val _cachedUserAge = MutableLiveData<String>()
+
     val userName = MutableLiveData<String>()
     val userAge = MutableLiveData<String>()
-
-    val cachedUserName = MutableLiveData<String>()
-    val cachedUserAge = MutableLiveData<String>()
 
     private val _showToastEvent = MutableLiveData<Event<String>>()
     val showToastEvent: LiveData<Event<String>> = _showToastEvent
@@ -46,15 +48,14 @@ class UserDataViewModel(app: Application) : AndroidViewModel(app) {
 
         interactor = UserEditInteractor(app)
 
-        //observe DB changes
+        //observe user data changes
         val dispos: Disposable = interactor.getUserObservable()
                 .subscribeBy { user ->
                     userName.value = user.name
                     userAge.value = if (user.age > 0) user.age.toString() else ""
 
-                    cachedUserName.value = userName.value
-                    cachedUserAge.value = userName.value
-
+                    _cachedUserName.value = userName.value
+                    _cachedUserAge.value = userAge.value
                 }
         subscriptions.add(dispos)
 
@@ -88,6 +89,26 @@ class UserDataViewModel(app: Application) : AndroidViewModel(app) {
                                 })
         subscriptions.add(disposable)
 
+    }
+
+    fun isUpdateButtonEnabled(): LiveData<Boolean> {
+        val isEnabled = MediatorLiveData<Boolean>()
+
+        isEnabled.addSource(userName) {
+            isEnabled.value = isUserDataChanged()
+        }
+
+        isEnabled.addSource(userAge) {
+            isEnabled.value = isUserDataChanged()
+        }
+
+        return isEnabled
+
+    }
+
+    private fun isUserDataChanged(): Boolean {
+        return (userName.value != _cachedUserName.value) ||
+                (userAge.value != _cachedUserAge.value)
     }
 
     private fun showToastMessage(message: String) {
