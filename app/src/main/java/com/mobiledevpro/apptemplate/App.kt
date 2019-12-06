95f4b46d17e47a73d23f61139ffef6dfc728196c
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2019-12-06 23:41:53 +0200

    Upgraded libraries

--- app/src/main/java/com/mobiledevpro/apptemplate/App.kt
+++ app/src/main/java/com/mobiledevpro/apptemplate/App.kt
@@ -3,7 +3,6 @@ package com.mobiledevpro.apptemplate
 import android.app.Application
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
-import com.squareup.leakcanary.LeakCanary
 import io.fabric.sdk.android.Fabric
 
 /**
@@ -21,13 +20,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return
-        }
-
-        LeakCanary.install(this)
 
         val crashlyticsKit = Crashlytics.Builder()
                 //.core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
