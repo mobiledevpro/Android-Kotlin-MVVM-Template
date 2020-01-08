8b1c2c3c368da021c987c5cf3e0e982cfcf040a5
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2020-01-08 15:37:44 +0200

    Use window insets to apply top & bottom paddings

--- app/src/main/java/com/mobiledevpro/apptemplate/ui/mainscreen/view/MainActivity.kt
+++ app/src/main/java/com/mobiledevpro/apptemplate/ui/mainscreen/view/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.mobiledevpro.apptemplate.ui.mainscreen.view
 
+import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.updatePadding
 import com.mobiledevpro.apptemplate.R
 import com.mobiledevpro.commons.activity.BaseActivity
 
 class MainActivity : BaseActivity() {
 
     override fun getLayoutResId() = R.layout.activity_main
+
+    override fun isAdjustFontScaleToNormal() = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //set navigation bar translucent
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        super.onCreate(savedInstanceState)
+
+        applyWindowInsets(findViewById(android.R.id.content))
+    }
 
     override fun initToolbar() {
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
@@ -21,5 +35,16 @@ class MainActivity : BaseActivity() {
     override fun populateView(layoutView: View) {
         //work with view
 
+    }
+
+    private fun applyWindowInsets(view: View) {
+        //Use Window Insets to set top and bottom paddings to our activity
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            v.updatePadding(
+                    bottom = insets.systemWindowInsetBottom,
+                    top = insets.systemWindowInsetTop
+            )
+            insets
+        }
     }
 }
