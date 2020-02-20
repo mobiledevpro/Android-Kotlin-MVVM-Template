e50ea1f5e4b14f833725f5507781fa64dec54640
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2020-02-20 13:58:09 +0200

    Upgrade to the Firebase Crashlytics SDK

--- app/src/main/java/com/mobiledevpro/app/App.kt
+++ app/src/main/java/com/mobiledevpro/app/App.kt
@@ -1,9 +1,6 @@
 package com.mobiledevpro.app
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
-import io.fabric.sdk.android.Fabric
 
 /**
  * Main application class
@@ -21,11 +18,5 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        val crashlyticsKit = Crashlytics.Builder()
-                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
-                //.core(CrashlyticsCore.Builder().build())
-                .build()
-        Fabric.with(this, crashlyticsKit)
     }
 }
