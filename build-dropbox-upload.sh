90d4e429a92c75f6b869b4ae5e8c989f23e3c153
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2020-05-09 18:28:05 +0300

    Setup circle CI

--- build-dropbox-upload.sh
+++ build-dropbox-upload.sh
@@ -2,7 +2,7 @@
 
 #put release apk to dropbox
 APK_FILES=./app/build/outputs/apk/*/*/*-release.apk
-DROPBOX_TOKEN="MS43qTgT_TAAAAAAAAAFkS6Ostvx69pBOjdn32WyXOLVb-wMeQ8x4HDetk4hW5Fa"
+DROPBOX_TOKEN="MS43qTgT_TAAAAAAAAAMp0Ecp_iBdKBib-4G_OXTjSHsnOofhn18Z-ZWZ_GCU45S"
 
 for FILE in $APK_FILES
 do
