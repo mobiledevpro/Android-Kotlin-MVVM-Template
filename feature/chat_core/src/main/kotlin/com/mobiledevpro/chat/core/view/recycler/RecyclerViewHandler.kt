3aac2fae75cea03d6705d177c562dac2661e1f2f
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2020-12-15 19:38:23 +0200

    Designed layout for chat messages

---
+++ feature/chat_core/src/main/kotlin/com/mobiledevpro/chat/core/view/recycler/RecyclerViewHandler.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 | Dmitri Chernysh | http://mobile-dev.pro
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.mobiledevpro.chat.core.view.recycler
+
+/**
+ * Interface for events handler for RecyclerView items
+ *
+ * Created on Dec 14, 2020.
+ *
+ */
+interface RecyclerViewHandler {
+    fun onClickItem(item: Any) {}
+}
