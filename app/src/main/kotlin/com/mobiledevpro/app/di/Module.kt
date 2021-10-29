6b2bdf02742b1594ff8071c1daa1fe241b309d60
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2021-10-29 23:59:22 +0300

    Added Gradle Doctor plugin to check the build before compiling. Resolved #33

--- app/src/main/kotlin/com/mobiledevpro/app/di/Module.kt
+++ app/src/main/kotlin/com/mobiledevpro/app/di/Module.kt
@@ -2,6 +2,7 @@ package com.mobiledevpro.app.di
 
 import com.mobiledevpro.app.helper.ImplResourcesProvider
 import com.mobiledevpro.app.helper.ResourcesProvider
+import com.mobiledevpro.database.di.coreDatabaseModule
 import com.mobiledevpro.rx.di.coreRxModule
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
@@ -13,7 +14,8 @@ import org.koin.dsl.module
  */
 fun getModules() = listOf(
     presentationCommonModule,
-    coreRxModule
+    coreRxModule,
+    coreDatabaseModule
 )
 
 val presentationCommonModule = module {
